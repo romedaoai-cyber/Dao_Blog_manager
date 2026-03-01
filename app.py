b284b67cf6c8ddb57a40a851f0f33f80d306ed38
Rome Ye <srs0319@gmail.com>
2026-03-01 13:48:57 -0800

    add streamlit app entry point

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from blog_content_generator import BlogContentGenerator
+from hubspot_blog_client import HubSpotBlogClient
+
+st.title("DaoAI Blog Manager")
+
+st.markdown("Welcome to the DaoAI Blog Manager. This tool allows you to generate blog content and publish it to HubSpot.")
